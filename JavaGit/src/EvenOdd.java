e1539508b44b6a085076ffbaec68274245632670
Hema <tripathihema1984@gmail.com>
2024-01-29 11:14:24 -0500

    Even Odd

---
+++ JavaGit/src/EvenOdd.java
@@ -0,0 +1,13 @@
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num = 30;
+		if(num%2==0)
+			System.out.println("This is an even number");
+		else
+			System.out.println("This is an odd number");
+
+	}
+
+}
